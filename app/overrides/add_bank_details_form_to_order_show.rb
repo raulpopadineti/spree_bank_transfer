a139ba05771ee3141238b4ffaf6597a67d0a4455
adoptanoob <adoptanoobinrails@gmail.com>
2015-04-05 12:57:17 +0200

    working but need to make that link permanent

--- app/overrides/add_bank_details_form_to_order_show.rb
+++ app/overrides/add_bank_details_form_to_order_show.rb
@@ -4,10 +4,8 @@ Deface::Override.new(
   :insert_bottom => ".payment-info",
   :text => %q{
     <% if bank_transfer_payment = @order.payments.from_bank_transfer.first %>
-      <% unless bank_transfer_payment.details_submitted? %>
         <br>
         <%= link_to Spree.t(:add_details), "https://docs.google.com/viewerng/viewer?url=https://s3-eu-west-1.amazonaws.com/spreeimagesproduits/RIBGUIXMODEL.pdf", :class => 'button add_bt_details', target: "_blank" %>
-      <% end %>
         <br>
     <% end %>
   }
