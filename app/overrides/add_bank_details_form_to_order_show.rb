aa0b43105c9e1641266480df061adacec7cd15ec
adoptanoob <adoptanoobinrails@gmail.com>
2015-04-05 12:51:19 +0200

    changed the order#show to include link to pdf

--- app/overrides/add_bank_details_form_to_order_show.rb
+++ app/overrides/add_bank_details_form_to_order_show.rb
@@ -6,27 +6,9 @@ Deface::Override.new(
     <% if bank_transfer_payment = @order.payments.from_bank_transfer.first %>
       <% unless bank_transfer_payment.details_submitted? %>
         <br>
-        <%= link_to Spree.t(:add_details), "javascript:void(0);", :class => 'button add_bt_details' %>
+        <%= link_to Spree.t(:add_details), "https://docs.google.com/viewerng/viewer?url=https://s3-eu-west-1.amazonaws.com/spreeimagesproduits/RIBGUIXMODEL.pdf", :class => 'button add_bt_details', target: "_blank" %>
       <% end %>
-      <%= form_for bank_transfer_payment, :html => { :class => "bt_payment_form #{bank_transfer_payment.details_submitted? ? '' : 'hidden'}" } do |f| %>
-        <table>
-          <tr>
-            <td><%= Spree.t(:deposited_on) %></td><td><%= f.text_field :deposited_on, :disabled => bank_transfer_payment.details_submitted?, :class => 'required_field', 'field-name' => 'Deposited on' %></td>
-          </tr>
-          <tr>
-            <td><%= Spree.t(:bank_name) %></td><td><%= f.text_field :bank_name, :disabled => bank_transfer_payment.details_submitted?, :class => 'required_field', 'field-name' => 'Bank name' %></td>
-          </tr>
-          <tr>
-            <td><%= Spree.t(:account_no) %></td><td><%= f.text_field :account_no, :disabled => bank_transfer_payment.details_submitted?, :class => 'required_field', 'field-name' => 'Account No.' %></td>
-          </tr>
-          <tr>
-            <td><%= Spree.t(:transaction_reference_no) %></td><td><%= f.text_field :transaction_reference_no, :disabled => bank_transfer_payment.details_submitted?, :class => 'required_field', 'field-name' => 'Transaction Reference No.' %></td>
-          </tr>
-        </table>
         <br>
-        <% unless bank_transfer_payment.details_submitted? %>
-          <%= f.submit "Submit", :confirm => Spree.t(:confirm_details_submit)  %>
-        <% end %>
       <% end %>
     <% end %>
   }
