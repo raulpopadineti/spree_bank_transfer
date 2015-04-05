0d9bec766ee23d0a1e3d3cc1b17ccd0fa1846f2a
adoptanoob <adoptanoobinrails@gmail.com>
2015-04-05 14:09:09 +0200

    added Spree.t before

--- app/overrides/add_bank_transfer_instructions_link_to_payment.rb
+++ app/overrides/add_bank_transfer_instructions_link_to_payment.rb
@@ -4,7 +4,7 @@ Deface::Override.new(
   :insert_after => "#payment-method-fields",
   :text => %q{
     <% if @order.available_payment_methods.any? { |pm| pm.type == 'Spree::PaymentMethod::BankTransfer' } %>
-      <p><%= link_to t(:how_bank_transfer_works), "https://docs.google.com/viewerng/viewer?url=https://s3-eu-west-1.amazonaws.com/spreeimagesproduits/RIBGUIXMODEL.pdf", :id => 'banktransfer_instructions', :target => "_blank" %></p>
+      <p><%= link_to Spree.t(:how_bank_transfer_works), "https://docs.google.com/viewerng/viewer?url=https://s3-eu-west-1.amazonaws.com/spreeimagesproduits/RIBGUIXMODEL.pdf", :id => 'banktransfer_instructions', :target => "_blank" %></p>
     <% end %>
   }
 )
